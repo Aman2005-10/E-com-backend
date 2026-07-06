44b9c961ea70b0a5a9261222e0daaa9fe655f4dc
Aman2005-10 <katariaaman43@gmail.com>
2026-07-06 13:23:05 +0530

    Add New Folders For MVC

---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const route   = express.Router()
+
+route.get('/user', (req, res) => {
+    res.send('User route is working')
+
+})
+
+export default route
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ dotenv.config()
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db.js'
+import userRoute from './routes/userRoute.js'
 const app = express()
 
 
 app.use(express.json())
 app.use(cors())
+app.use('/api', userRoute)
 
 connectDB()
 
