03318d33a02177f4670b6eb401554ce581b4717e
Aman2005-10 <katariaaman43@gmail.com>
2026-07-08 11:52:02 +0530

    test bacekend

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,7 +8,7 @@ const route  = express.Router();
 
 route.post("/create-product" , isSignedIn  , isAdmin , upload.single("image") , createProduct)
 route.delete("/delete-product/:id" , isSignedIn , isAdmin , deleteController)
-route.get("/get-all" , isSignedIn ,  getAllProductCont)
+route.get("/get-all"  ,  getAllProductCont)
 route.get("/get-one/:id" , isSignedIn , getProductById)
 
 
